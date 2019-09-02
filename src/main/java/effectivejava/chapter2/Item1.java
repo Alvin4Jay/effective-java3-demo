750236aadbfa61baff0906bd8bcadcec77784687
xuanjian <xuanjian@wacai.com>
2019-09-02 10:07:51 +0800

    item1: 使用静态工厂方法而不是构造器

---
+++ src/main/java/effectivejava/chapter2/Item1.java
@@ -0,0 +1,46 @@
+package effectivejava.chapter2;
+
+import java.io.BufferedReader;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Date;
+import java.util.EnumSet;
+
+/**
+ * item1: 用静态工厂方法替代构造器
+ *
+ * @author xuanjian
+ */
+public class Item1 {
+
+    public static void main(String[] args) throws Throwable {
+        // from类型转换方法
+        Instant now = Instant.now();
+        Date nowDate = Date.from(now);
+        System.out.println(nowDate.toString());
+
+        // of聚合方法
+        EnumSet<Rank> faceCards = EnumSet.of(Rank.JACK, Rank.QUEUE, Rank.KING);
+
+        // valueOf
+        BigInteger prime = BigInteger.valueOf(Integer.MAX_VALUE);
+
+        // create和newInstance
+        Object newArray = Array.newInstance(int.class, 10);
+
+        // getType
+        FileStore fs = Files.getFileStore(Paths.get("/"));
+
+        // newType
+        BufferedReader br = Files.newBufferedReader(Paths.get("/"));
+    }
+
+    private enum Rank {
+        JACK, QUEUE, KING
+    }
+
+}
